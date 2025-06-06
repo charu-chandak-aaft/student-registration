9d1ea667c5501f245ca376eea3219d543c96cdae
charu-chandak-aaft <charu.c@aaft.com>
2025-06-06 17:44:33 +0530

    fix

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { db } from '@/app/lib/db';
+import { db } from '../../../lib/db';
 
 export async function GET() {
   const [rows] = await db.execute('SELECT * FROM student_registration');
